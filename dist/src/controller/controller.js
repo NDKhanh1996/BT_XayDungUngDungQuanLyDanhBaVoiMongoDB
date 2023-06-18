9105e1ae1de28b54f249c8722be831c49842c6c8
Duy Khánh Nguyễn <ndkhanh1996@gmail.com>
2023-06-18 16:57:42 +0700

    add

--- dist/src/controller/controller.js
+++ dist/src/controller/controller.js
@@ -4,8 +4,11 @@ exports.Controller = void 0;
 const contacts_model_1 = require("../schema/contacts.model");
 class Controller {
     static async getContacts(req, res) {
+        var _a, _b;
         try {
-            const contactsList = await contacts_model_1.contacts.find();
+            const offset = ((_a = req.query) === null || _a === void 0 ? void 0 : _a.offset) || '0';
+            const limit = ((_b = req.query) === null || _b === void 0 ? void 0 : _b.limit) || '3';
+            const contactsList = await contacts_model_1.contacts.find().skip(+offset).limit(+limit);
             (contactsList.length == 0) ? res.send('there are no contacts') : res.render('contactsList', { contactsList: contactsList });
         }
         catch (error) {
--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,15 +1,17 @@
 import { contacts } from "../schema/contacts.model";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 export class Controller {
-    static async getContacts(req: Request, res: Response){
+    static async getContacts(req: Request, res: Response) {
         try {
-            const contactsList = await contacts.find();
+            const offset = req.query?.offset || '0';
+            const limit = req.query?.limit || '3';
+            const contactsList = await contacts.find().skip(+offset).limit(+limit);
             (contactsList.length == 0) ? res.send('there are no contacts') : res.render('contactsList', { contactsList: contactsList });
         } catch (error) {
             res.render(error.message);
         }
     }
-    static async showAddContact(req: Request, res: Response){
+    static async showAddContact(req: Request, res: Response) {
         try {
             res.render('addContact');
         } catch (error) {
